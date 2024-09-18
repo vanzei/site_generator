935829d545357182b1a8dd0e48d754a4d7020577
vanzei <leonardo.vanzei123@gmail.com>
2024-09-18 07:37:51 -0400

    htmlnode and test

---
+++ src/htmlnode.py
@@ -0,0 +1,19 @@
+class HTMLNode:
+    def __init__(self, tag = None, value = None, children = None,props = None):
+        self.tag = tag
+        self.value = value
+        self.children = children if children is not None else []
+        self.props = props if props is not None else {}
+    
+    def __repr__(self):
+        return (f"HTMLNode(tag={self.tag!r}, value={self.value!r}, "
+                f"children={len(self.children)} children, props={self.props!r})")
+
+
+    def to_html(self):
+        raise NotImplementedError("Subclasses should implement this method")
+    
+    def props_to_html(self):
+        if not self.props:
+            return  ""
+        return " " + " ".join(f'{key}="{value}"' for key, value in self.props.items())
